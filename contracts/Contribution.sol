cdaaac065a8e62a2a12a090072d1168006a17299
RCYP <yohann.pereira28@gmail.com>
2019-05-30 09:56:42 -0400

    token converter function

--- contracts/Contribution.sol
+++ contracts/Contribution.sol
@@ -7,11 +7,13 @@ contract Contribution{
 using SafeMath for uint256;
     //@dev amount of ether donated for each address
     mapping(address=>uint) private userEtherDonated;
+    uint private ethToTokenRate;
     Token private tokenContractReference;
     event DonationMade(address donator,uint amountDonated,uint tokensAquired);
 
-    constructor(address _tokenReference) public{
+    constructor(address _tokenReference, uint _ethToTokenRate) public{
         tokenContractReference = Token(_tokenReference);
+        ethToTokenRate = _ethToTokenRate;
     }
     //@notice assuming donating 1ether will return 1 token
     //@dev function first checks if the amount is not 0, then adds amount to 'userEtherDonated' mapping then returns the tokens
@@ -20,8 +22,8 @@ using SafeMath for uint256;
     function donate(uint amountInEther) external payable{
         require(amountInEther != 0, 'You donated 0 ether, hence nothing was done :(');
         userEtherDonated[msg.sender] = userEtherDonated[msg.sender].add(amountInEther);
-        uint tokensToGet = amountInEther;
-        require(tokenContractReference.mint(msg.sender,tokensToGet));
+        uint tokensToGet = tokensToMint(amountInEther);
+        revert(tokenContractReference.mint(msg.sender,tokensToGet));
         emit DonationMade(msg.sender,amountInEther,tokensToGet);
     }
 
@@ -31,8 +33,14 @@ using SafeMath for uint256;
     function getDonationByAddress(address walletAddress) public view returns(uint){
         return userEtherDonated[walletAddress];
     }
-    
+
+    //@dev get the address of the Token contract that this contract refers to
     function getTokenContractAddress() public view returns(address) {
-	return address(tokenContractReference);    
-}
+	return address(tokenContractReference);
+    }
+
+    //@dev a private view function that converts ether to tokens to be minted
+    function tokensToMint(uint amountInEther) private view returns(uint) {
+        return ethToTokenRate * amountInEther;
+    }
 }
